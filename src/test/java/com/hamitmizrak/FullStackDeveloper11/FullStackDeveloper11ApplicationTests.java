eb7149555eb4a05af03f6e16448423713264f42f
Hamit Mizrak <hamitmizrak@gmail.com>
2023-10-08 09:54:50 +0300

    Initial commit

---
+++ src/test/java/com/hamitmizrak/FullStackDeveloper11/FullStackDeveloper11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.FullStackDeveloper11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDeveloper11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
