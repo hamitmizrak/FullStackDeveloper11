28f5cb47550407f18539ac83019d23ccf72075fd
Hamit Mizrak <hamitmizrak@gmail.com>
2023-10-08 10:35:13 +0300

    All Files added

--- src/main/java/com/hamitmizrak/FullStackDeveloper11/FullStackDeveloper11Application.java
+++ src/main/java/com/hamitmizrak/FullStackDeveloper11/FullStackDeveloper11Application.java
@@ -2,8 +2,37 @@ package com.hamitmizrak.FullStackDeveloper11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+// @EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+// @EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository") //Repository bulamadığı zaman
+
+// Spring Cache aktif etmek gerekiyor.
+// @EnableCaching
+
+// Auditing Aktif etmek
+// @EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme ancak Spring security için gerekli kütüphaneleri dahil
+@SpringBootApplication(exclude = {
+		//SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+
+// @SpringBootApplication
 public class FullStackDeveloper11Application {
 
 	public static void main(String[] args) {
