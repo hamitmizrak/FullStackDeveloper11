68da6b01b2d0fbc8c6c128f6aca5106ed08d16e5
Hamit Mizrak <hamitmizrak@gmail.com>
2023-10-09 18:24:47 +0300

    Dto,Entity,Repository

---
+++ src/main/java/com/hamitmizrak/FullStackDeveloper11/business/dto/RegisterDto.java
@@ -0,0 +1,73 @@
+package com.hamitmizrak.FullStackDeveloper11.business.dto;
+
+import com.hamitmizrak.FullStackDeveloper11.audit.AuditingAwareBaseDto;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import java.io.Serializable;
+
+// LOMBOK
+@Data
+@Log4j2
+@Builder
+
+// REGISTER
+public class RegisterDto extends AuditingAwareBaseDto implements Serializable {
+
+    // Serileştirme
+    public static final Long serialVersionUID=1L;
+
+    // Global Variable (6)
+    // Dikkat: message sonunda boşluk olmasın
+    @NotEmpty(message = "{register.username.validation.constraints.NotNull.message}")
+    private String registerUsername;
+    @NotEmpty(message = "{register.name.validation.constraints.NotNull.message}")
+    private String registerName;
+
+    @NotEmpty(message = "{register.surname.validation.constraints.NotNull.message}")
+    private String registerSurname;
+
+    @NotEmpty(message = "{register.email.validation.constraints.NotNull.message}")
+    private String registerEmail;
+
+    @NotEmpty(message = "{register.password.validation.constraints.NotNull.message}")
+    private String registerPassword;
+
+    @Builder.Default //default olarak kullanıcı pasif olsun admin bunu aktif yapsın
+    private Boolean registerIsPassive=false;
+
+    //parametresiz constructor
+    public RegisterDto() {
+    }
+
+    //parametreli constructor
+    public RegisterDto(String registerUsername, String registerName, String registerSurname, String registerEmail, String registerPassword, Boolean registerIsPassive) {
+        this.registerUsername = registerUsername;
+        this.registerName = registerName;
+        this.registerSurname = registerSurname;
+        this.registerEmail = registerEmail;
+        this.registerPassword = registerPassword;
+        this.registerIsPassive = registerIsPassive;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "RegisterDto{" +
+                "registerUsername='" + registerUsername + '\'' +
+                ", registerName='" + registerName + '\'' +
+                ", registerSurname='" + registerSurname + '\'' +
+                ", registerEmail='" + registerEmail + '\'' +
+                ", registerPassword='" + registerPassword + '\'' +
+                ", registerIsPassive='" + registerIsPassive + '\'' +
+                ", id=" + id +
+                ", systemDate=" + systemDate +
+                ", createdUser='" + createdUser + '\'' +
+                ", createdDate=" + createdDate +
+                ", lastUser='" + lastUser + '\'' +
+                ", lastDate=" + lastDate +
+                '}';
+    } //end toString
+
+} //end class
---
+++ src/main/java/com/hamitmizrak/FullStackDeveloper11/data/repository/IRegisterRepository.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak.FullStackDeveloper11.data.repository;
+
+import com.hamitmizrak.FullStackDeveloper11.data.entity.RegisterEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+// CrudRepository
+// JpaRepository
+// PagingAndSortingRepository
+@Repository
+public interface IRegisterRepository extends CrudRepository<RegisterEntity,Long> {
+
+    // Delivered Query
+    Optional<RegisterEntity> findByRegisterEmail(String email);
+}
