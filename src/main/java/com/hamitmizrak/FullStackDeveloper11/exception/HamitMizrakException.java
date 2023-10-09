8efabd711bc08bc966c2d532c1df3ecc2584feff
Hamit Mizrak <hamitmizrak@gmail.com>
2023-10-09 15:29:50 +0300

    status

--- src/main/java/com/hamitmizrak/FullStackDeveloper11/exception/HamitMizrakException.java
+++ src/main/java/com/hamitmizrak/FullStackDeveloper11/exception/HamitMizrakException.java
@@ -3,8 +3,10 @@ package com.hamitmizrak.FullStackDeveloper11.exception;
 // 201 : oluşturuldu (Created)
 // 400 : Kötü istek (Bad Request)
 // 401 : Yetkisiz Giriş ( Authorized)
+// 402 : Ödeme ( Payment Required)
 // 403 : Yasak Giriş ( Forbidden)
 // 404 : Bulunamadı (not found)
+// 502 :            (Bad Gateway)
 
 // My Special Exception
 public class HamitMizrakException extends RuntimeException{
