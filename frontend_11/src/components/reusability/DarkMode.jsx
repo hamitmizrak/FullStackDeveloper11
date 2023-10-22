10d0012c2065b5f8c1a86163d1c7167762d60770
HamitMizrak <hamitmizrak@gmail.com>
2023-10-22 12:41:07 +0300

    Dark Mode

--- frontend_11/src/components/reusability/DarkMode.jsx
+++ frontend_11/src/components/reusability/DarkMode.jsx
@@ -1,30 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { DarkModeToggle } from '@anatoliygatt/dark-mode-toggle';
 
+
+// import { Link } from 'react-router-dom';
+// import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+// npm install react-toggle-dark-mode
+// npm install @anatoliygatt/dark-mode-toggle @emotion/react @emotion/styled
 // Function
 function DarkMode() {
 
-    // Css styleDarkMode
-    const styleDarkMode = () => {
-        // css
-        const css_dark = {
-            backgroundColor: "#000000",
-            color: "#ffffff",
-        }
+    const [mode, setMode] = useState('dark');
+    //1 YOL
+    // const[darkMode,setDarkMode]=React.useState(false);
+    // const toggleDarkMode=(checked)=>{
+    //     setDarkMode(checked)
+    // };
 
-        let lightDarkMode = document.body.classList.toggle("css_dark")
-        console.log("css_dark");
-    };
 
     return (
         <React.Fragment>
             {/* Register / Login */}
             <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
 
-                <li className="nav-item">
-                    {/* Root: relative Path */}
-                    <Link className="nav-link" onClick={styleDarkMode}><i className="fa-solid fa-circle-half-stroke"></i> </Link>
+                {/* <li className="nav-item">
+                    <DarkModeSwitch
+                    style={{marginTop:'1rem',color:"white"}}
+                    checked={darkMode}
+                    onChange={toggleDarkMode}
+                    size={40}
+                    />
+               </li>  */}
+
+                <li>
+                    <DarkModeToggle
+                        mode={mode}
+                        dark="Dark"
+                        light="Light"
+                        size="sm"
+                        inactiveTrackColor="#e2e8f0"
+                        inactiveTrackColorOnHover="#f8fafc"
+                        inactiveTrackColorOnActive="#cbd5e1"
+                        activeTrackColor="#334155"
+                        activeTrackColorOnHover="#1e293b"
+                        activeTrackColorOnActive="#0f172a"
+                        inactiveThumbColor="#1e293b"
+                        activeThumbColor="#e2e8f0"
+                        onChange={(mode) => {
+                            setMode(mode);
+                        }}
+                    />
                 </li>
+
             </ul>
         </React.Fragment>
     )
